d172bd9d74350385e749240bba11a6080cf965e8
Maxim Dushevsky <dushevskymax@mail.ru>
2022-06-06 03:15:00 +0300

    Lab3 done

---
+++ Genres/Drama.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    [Serializable]
+    internal class Drama:Film, ISerializable
+    {
+        public override string Name { get; set; } = "";
+
+        public override int Year { get; set; }
+
+        public override string Director { get; set; } = "";
+
+        
+
+        public Drama()
+        {
+        }
+
+        public override string Work()
+        {
+            return "It's so touching";
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            
+        }
+
+        
+    }
+}
